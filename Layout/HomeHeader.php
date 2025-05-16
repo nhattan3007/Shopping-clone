f50f3fcf9bf7b094ee05559782a40f9f0072728e
son <sonnguyenminh13231@gmail.com>
2025-05-16 16:59:13 +0700

    add banner

--- Layout/HomeHeader.php
+++ Layout/HomeHeader.php
@@ -21,14 +21,14 @@ if (session_status() === PHP_SESSION_NONE) {
         <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous"> -->
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="<?= $baseURL . "/Assets/admin/css/styles.css"?> " rel="stylesheet" />
+        <link href="<?= $baseURL . "/Assets/css/banner.css"?>" rel="stylesheet" />
     </head>
     <body>
         
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
-            <a class="navbar-brand fw-bold" href="<?=$baseURL?>Product/index" style="font-size: 1.8rem;"> </a>
-            <img src="<?=$baseURL?>/assets/images/2.png " alt="top sent" height="60">
+            <a class="navbar-brand fw-bold" href="<?=$baseURL?>Product/index" style="font-size: 1.8rem;">ShoppingQST</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
@@ -84,11 +84,3 @@ if (session_status() === PHP_SESSION_NONE) {
             </div>
         </nav>
         <!-- Header-->
-        <header class="bg-dark py-5">
-            <div class="container px-4 px-lg-5 my-5">
-                <div class="text-center text-white" >
-                    <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
-                </div>
-            </div>
-        </header>
